947e0091efe39d8fdd630757898c22ced73f5b32
jbmattos03 <jbmattos2003@gmail.com>
2025-04-22 11:31:55 -0300

    feat: :sparkles: Update main.py
    Added FitnessFunction initialization and print the best solution, its weight and its fitness value

--- src/main.py
+++ src/main.py
@@ -4,12 +4,30 @@ It creates an instance of the GeneticAlgorithm class and runs the algorithm.
 """
 
 from genetic_algorithm import GeneticAlgorithm
+from fitness_function import FitnessFunction
+
+import random
 
 def main():
-    gen_alg = GeneticAlgorithm()
-    gen_alg.initialize_population(size=10, chromosome_length=3)
+    fitness_function = FitnessFunction(max_sum=15, 
+                                       weight_values=[2, 3, 4, 5, 7, 1, 6, 4.5, 3.5, 2.5], 
+                                       price_values=[40, 50, 65, 80, 110, 15, 90, 70, 60, 55])
+    
+    gen_alg = GeneticAlgorithm(fitness_function=fitness_function)
+    gen_alg.initialize_population(size=50, chromosome_length=10)
 
-    #gen_alg.run()
+    gen_alg.run(generations=200, 
+                parents_qty=40,
+                verbose=False
+                )
+
+    solution = gen_alg.get_best_solution()
+
+    print("==========================================================")
+    print(f"Best solution: {[g.value for g in solution.values]}")
+    print(f"Weight: {sum([solution.values[i].value*fitness_function.weight_values[i] for i in range(solution.length)])}")
+    print(f"Fitness: {solution.fitness}")
+    print("==========================================================")
 
 if __name__ == "__main__":
     main()
