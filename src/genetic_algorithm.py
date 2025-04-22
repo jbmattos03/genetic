6bee965d9ba51ffc32eafcfdfc84b3d7c309265a
jbmattos03 <jbmattos2003@gmail.com>
2025-04-22 11:30:20 -0300

    fix: :bug: Fix genetic_algorithm.py
    Fixed bugs related to crossover, mutation and evaluate_population that allowed invalid chromosomes to be added to the population

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -11,11 +11,8 @@ from fitness_function import FitnessFunction
 
 import random
 
-seed = 42
-random.seed(seed)  # Set the seed for reproducibility
-
 class GeneticAlgorithm:
-    def __init__(self, mutation_rate: float = 0.01, crossover_rate: float = 0.7, fitness_function: FitnessFunction = None):
+    def __init__(self, mutation_rate: float = 0.05, crossover_rate: float = 0.8, fitness_function: FitnessFunction = None):
         self.population = None
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
@@ -31,7 +28,7 @@ class GeneticAlgorithm:
         """
         population = []
 
-        range_values = self.fitness_function.range_values
+        weight_values = self.fitness_function.weight_values
         max_sum = self.fitness_function.max_sum
 
         for _ in range(size):
@@ -39,11 +36,11 @@ class GeneticAlgorithm:
             success_flag = False
 
             while success_flag is False:
-                for i in range(chromosome_length):
-                    gene = Gene(random.randint(0, range_values[i]))
+                for _ in range(chromosome_length):
+                    gene = Gene(random.randint(0,1)) # Randomly generate a gene (0 or 1)
                     chromosome.values.append(gene)
                 
-                if chromosome not in population and sum([chromosome.values[i].value * range_values[i] for i in range(chromosome_length)]) <= max_sum:
+                if chromosome not in population and sum([chromosome.values[i].value * weight_values[i] for i in range(chromosome_length)]) <= max_sum:
                     success_flag = True
                 else:
                     chromosome.values = []
@@ -58,8 +55,12 @@ class GeneticAlgorithm:
         :param fitness_function: The function used to evaluate the fitness of a chromosome.
         """
         for chromosome in self.population.chromosomes:
+            fitness = 0
+
             for i in range(chromosome.length):
-                chromosome.fitness += chromosome.values[i].value * self.fitness_function.price_values[i]
+                fitness += chromosome.values[i].value * self.fitness_function.price_values[i]
+            
+            chromosome.fitness = fitness
                     
             # Ensure fitness is non-negative (unlikely, but just in case)
             if chromosome.fitness < 0:
@@ -80,58 +81,89 @@ class GeneticAlgorithm:
         total_fitness = sum([chromosome.fitness for chromosome in self.population.chromosomes])
         current_sum = 0
 
-        while current_pop != [] and len(selected) < parents:
-            selected_flag = False
+        while len(selected) < parents:
             random_number = random.randint(0, total_fitness)
             
             for chromosome in current_pop:
                 current_sum += chromosome.fitness
 
                 if random_number < current_sum:
-                    selected_flag = True
                     selected.append(chromosome)
-                    break
-            
-            if selected_flag is True:
-                current_pop.remove(chromosome)
+                    current_pop.remove(chromosome) # Remove parents
+                    break # Break out of for loop
+                
             current_sum = 0
-
-
-        # Sort selected parents by fitness
-        selected.sort(key=lambda x: x.fitness, reverse=True)
 
         # Group selected parents into pairs
         selected = [selected[i:i + 2] for i in range(0, len(selected), 2)]
     
         return selected
 
-    def crossover(self, parent1: Chromosome, parent2: Chromosome, chromosome_length: int = 3):
+    def crossover(self, parent1: Chromosome, parent2: Chromosome, chromosome_length: int = 10, attempts: int = 3, verbose: bool = True):
         """
         Performs crossover between two parents to create two offspring.
         :param parent1: The first parent chromosome.
         :param parent2: The second parent chromosome.
-        :return: A tuple containing the two offspring chromosomes.
         """
-        index = random.randint(1, parent1.length - 1)
-        print("Index:", index)
+        valid_flag = False
+        attempts_counter = attempts
 
-        offspring1 = Chromosome(parent2.values[:index] + parent1.values[index:], chromosome_length)
-        offspring2 = Chromosome(parent1.values[:index] + parent2.values[index:], chromosome_length)
+        while valid_flag is False and attempts_counter > 0:
+            attempts_counter -= 1
+
+            # Randomly select crossover points
+            i = random.randint(1, parent1.length - 1)
+            j = random.randint(1, parent2.length - 1)
+
+            if i > j:
+                offspring1 = Chromosome([Gene(g.value) for g in (parent2.values[:i] + parent1.values[i:])], chromosome_length)
+                offspring2 = Chromosome([Gene(g.value) for g in (parent1.values[:i] + parent2.values[i:])], chromosome_length)
+            elif j > i:
+                offspring1 = Chromosome([Gene(g.value) for g in (parent1.values[:j] + parent2.values[j:])], chromosome_length)
+                offspring2 = Chromosome([Gene(g.value) for g in (parent2.values[:j] + parent1.values[j:])], chromosome_length)
+            else:
+                offspring1 = Chromosome([Gene(g.value) for g in parent1.values], chromosome_length)
+                offspring1.values[i] = parent2.values[i]
+
+                offspring2 = Chromosome([Gene(g.value) for g in parent2.values], chromosome_length)
+                offspring2.values[j] = parent1.values[j]
+
+            # Check if the offspring are valid (i.e., their weight does not exceed the max sum)
+            # and if they are not already in the population
+            if offspring1 not in self.population.chromosomes and offspring2 not in self.population.chromosomes and \
+            sum([offspring1.values[k].value * self.fitness_function.weight_values[k] for k in range(chromosome_length)]) <= self.fitness_function.max_sum and \
+            sum([offspring2.values[k].value * self.fitness_function.weight_values[k] for k in range(chromosome_length)]) <= self.fitness_function.max_sum:
+                valid_flag = True
         
-        parent1.values = offspring1.values
-        parent2.values = offspring2.values
+        if valid_flag is True:
+            # Replace the parents with the offspring
+            parent1.values = offspring1.values
+            parent2.values = offspring2.values
+        elif valid_flag is False and verbose == True:
+            print(f"Crossover failed for parents {[g.value for g in parent1.values]} and {[g.value for g in parent2.values]} after {attempts} attempts.")
+            
 
-    def mutate(self, chromosome: Chromosome):
+    def mutate(self, chromosome: Chromosome, verbose: bool = True):
         """
         Mutates a chromosome with a certain probability.
         :param chromosome: The chromosome to mutate.
         """
-        for g in chromosome.values:
-            if round(random.random(), 2) <= self.mutation_rate:
-                new_value = random.randint(0, self.fitness_function.range_values[chromosome.values.index(g)])
+        for i, g in enumerate(chromosome.values):
+            if random.random() <= self.mutation_rate:
+                original_value = g.value
+                new_value = random.randint(0, 1)
 
                 if new_value != g.value:
                     g.value = new_value
+
+                    # Check if the mutation is valid (i.e., the weight does not exceed the max sum)
+                    if any(chromosome.values == pop_chromosome.values for pop_chromosome in self.population.chromosomes) or \
+                       sum([chromosome.values[k].value * self.fitness_function.weight_values[k] for k in range(chromosome.length)]) > self.fitness_function.max_sum:
+                        g.value = original_value
+
+                        if verbose is True:
+                            print(f"Mutation reverted for gene {i} in chromosome {[gene.value for gene in chromosome.values]}.")
+
 
     def get_best_solution(self) -> Chromosome:
         """
@@ -140,11 +172,32 @@ class GeneticAlgorithm:
         """
         return self.best
 
-    def run(self, generations: int):
+    def run(self, generations: int, parents_qty: int, verbose: bool = True):
         """
         Runs the genetic algorithm for a specified number of generations.
         :param generations: The number of generations to run the algorithm.
-        :param fitness_function: The function used to evaluate the fitness of a chromosome.
+        :param parents: The number of parents to select for crossover.
+        :param verbose: Whether to print the progress of the algorithm or not.
         """
-        pass
+        for _ in range(generations):
+            # Increase generation count
+            self.generation += 1
+
+            # Get fitness values for each chromosome
+            self.evaluate_population()
+
+            # Get parents and do crossover
+            parents = self.roulette(parents=parents_qty)
+            for parent_tuple in parents:
+                if random.random() <= self.crossover_rate:
+                    self.crossover(parent_tuple[0], parent_tuple[1], verbose=verbose)
+            
+            # Do mutation
+            for chromosome in self.population.chromosomes:
+                self.mutate(chromosome, verbose=verbose)
+
+            if verbose is True:
+                print(f"Generation: {self.generation}")
+                print(f"Best fitness: {self.best.fitness}")
+                print(f"Best chromosome: {[g.value for g in self.best.values]}")
 
