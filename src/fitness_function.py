964d2e3c544c315d58e424e1d5809125d981944b
jbmattos03 <jbmattos2003@gmail.com>
2025-04-22 11:28:49 -0300

    refactor: :bulb: Update fitness_function.py
    Renamed variables and updated comments

--- src/fitness_function.py
+++ src/fitness_function.py
@@ -3,15 +3,13 @@ This module defines the FitnessFunction class, which is used to evaluate the fit
 """
 
 class FitnessFunction:
-    def __init__(self, max_sum: int,  cost_values: list[int], range_values: list[int], price_values: list[int]):
+    def __init__(self, max_sum: int,  weight_values: list[int], price_values: list[int]):
         """
         Initializes the fitness function with the given parameters.
         :param max_sum: The maximum sum constraint for the fitness function.
-        :param range_values: The range of values for each gene in the chromosome (constraints for x1, ..., xn).
+        :param weight_values: The weight of values for each gene in the chromosome (constraints for x1, ..., xn).
         :param price_values: The price values associated with each gene in the chromosome.
-        :param cost_values: The cost values associated with each gene in the chromosome.
         """
         self.max_sum = max_sum
-        self.cost_values = cost_values
-        self.range_values = range_values
+        self.weight_values = weight_values
         self.price_values = price_values
