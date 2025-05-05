e2e3ef32aa687237a38ff616be1e4cf1ae16c592
jbmattos03 <jbmattos2003@gmail.com>
2025-05-05 20:42:47 -0300

    feat: :sparkles: Add qty_values attribute

--- src/main.py
+++ src/main.py
@@ -2,22 +2,20 @@
 This is the main module for the genetic algorithm.
 It creates an instance of the GeneticAlgorithm class and runs the algorithm.
 """
-
 from genetic_algorithm import GeneticAlgorithm
 from fitness_function import FitnessFunction
 
-import random
-
 def main():
-    fitness_function = FitnessFunction(max_sum=15, 
-                                       weight_values=[2, 3, 4, 5, 7, 1, 6, 4.5, 3.5, 2.5], 
+    fitness_function = FitnessFunction(max_sum=50,
+                                       qty_values=[5, 4, 3, 2, 1, 6, 2, 3, 4, 5],
+                                       weight_values=[2, 3, 4, 5, 7, 1, 6, 4.5, 3.5, 2.5],
                                        price_values=[40, 50, 65, 80, 110, 15, 90, 70, 60, 55])
     
     gen_alg = GeneticAlgorithm(fitness_function=fitness_function)
-    gen_alg.initialize_population(size=50, chromosome_length=10)
+    gen_alg.initialize_population(size=350, chromosome_length=10)
 
-    gen_alg.run(generations=200, 
-                parents_qty=40,
+    gen_alg.run(generations=1000, 
+                parents_qty=280,
                 verbose=False
                 )
 
